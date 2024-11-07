d10c0ad0501d083455c54fb8c76724b6927a947d
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-07 15:17:34 +0300

    linter module

--- learnify/urls.py
+++ learnify/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("learning/", include("lms.urls", namespace='learning')),
-    path("users/", include("users.urls", namespace='users'))
-
+    path("learning/", include("lms.urls", namespace="learning")),
+    path("users/", include("users.urls", namespace="users")),
 ]
