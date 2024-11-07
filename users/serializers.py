b82bfd870b845194daaf96c842dac7a2b9bc25cb
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-07 15:22:14 +0300

    add payment field UserSerialize, linter module

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ("id", "course", "lesson", "pay_day", "pay_method")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True, source="payment")
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("id", "email", "first_name", "last_name", "payments")
