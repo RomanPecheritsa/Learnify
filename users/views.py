3ce8be3fe7a6052ce0bc5417b9959dfc27c22c0b
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-14 12:55:03 +0300

    restructure UserAPI, add permissions, linter module

--- users/views.py
+++ users/views.py
@@ -1,18 +1,65 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import ListAPIView
 
-from users.models import User, Payment
-from users.serializers import UserSerializer, PaymentSerializer
+from users.models import Payment, User
+from users.serializers import (PaymentSerializer, UserHiddenSerializer,
+                               UserSerializer)
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(self.request.data.get("password"))
+        user.save()
 
-class PaymentListAPIView(ListAPIView):
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserHiddenSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_update(self, serializer):
+        if serializer.instance != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this profile.")
+
+        user = serializer.save()
+
+        new_password = self.request.data.get("password")
+        if new_password:
+            user.set_password(new_password)
+        user.save()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserHiddenSerializer
+
+    def get_serializer_class(self):
+        if self.get_object() == self.request.user:
+            return UserSerializer
+        return UserHiddenSerializer
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserHiddenSerializer
+
+    def perform_destroy(self, instance):
+        if instance != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this profile.")
+        instance.delete()
+
+
+class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
