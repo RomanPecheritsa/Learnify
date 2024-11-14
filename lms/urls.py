53f7e1fffcedf17e531a9e9f99f216857c4e3f81
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-14 12:46:16 +0300

    rename path name, linter module

--- lms/urls.py
+++ lms/urls.py
@@ -2,23 +2,19 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from lms.apps import LmsConfig
-from lms.views import (
-    CourseViewSet,
-    LessonListCreateAPIView,
-    LessonRetrieveUpdateDestroyAPIView,
-)
-
+from lms.views import (CourseViewSet, LessonListCreateAPIView,
+                       LessonRetrieveUpdateDestroyAPIView)
 
 app_name = LmsConfig.name
 
 router = DefaultRouter()
-router.register("courses", CourseViewSet, basename="course")
+router.register("courses", CourseViewSet, basename="courses")
 
 urlpatterns = [
-    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson-list"),
+    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson_list"),
     path(
         "lessons/<int:pk>/",
         LessonRetrieveUpdateDestroyAPIView.as_view(),
-        name="lesson-detail",
+        name="lesson_detail",
     ),
 ] + router.urls
