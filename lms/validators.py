2835cc968a48483125a9f8a52520d58e65b0eb0c
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-24 13:47:22 +0300

    linter module

--- lms/validators.py
+++ lms/validators.py
@@ -4,4 +4,6 @@ from rest_framework.exceptions import ValidationError
 def validate_youtube_only(value):
     allowed_names = ["youtube.com", "youtu.be"]
     if not any(name in value.lower() for name in allowed_names):
-        raise ValidationError("Ссылка должна быть только на ресурсы YouTube (youtube.com или youtu.be)")
+        raise ValidationError(
+            "Ссылка должна быть только на ресурсы YouTube (youtube.com или youtu.be)"
+        )
