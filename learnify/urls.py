f7527be0fc251904b6ad0d93c88ea34696a57125
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-24 13:46:44 +0300

    add schema, swagger, redoc path

--- learnify/urls.py
+++ learnify/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 
 urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("admin/", admin.site.urls),
     path("learning/", include("lms.urls", namespace="learning")),
     path("users/", include("users.urls", namespace="users")),
