e68d83205cc26b315e3c56cf629ad79e14d000d7
pecheritsa <pecheritsa.roman@gmail.com>
2024-11-24 13:52:58 +0300

    changed path payment

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import (PaymentListAPIView, SubscriptionAPIView,
+from users.views import (PaymentCreateAPIView, SubscriptionAPIView,
                          UserCreateAPIView, UserDestroyAPIView,
                          UserListAPIView, UserRetrieveAPIView,
                          UserUpdateAPIView)
@@ -21,7 +21,6 @@ urlpatterns = [
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
     path("edit/<int:pk>/", UserUpdateAPIView.as_view(), name="user_edit"),
     path("delete/<int:pk>/", UserDestroyAPIView.as_view(), name="user_delete"),
-    path("payment/", PaymentListAPIView.as_view(), name="user_payments"),
     path(
         "login/",
         TokenObtainPairView.as_view(permission_classes=[AllowAny]),
@@ -33,4 +32,5 @@ urlpatterns = [
         name="token_refresh",
     ),
     path("subs/", SubscriptionAPIView.as_view(), name="subscription"),
+    path("payment/", PaymentCreateAPIView.as_view(), name="payments"),
 ]
